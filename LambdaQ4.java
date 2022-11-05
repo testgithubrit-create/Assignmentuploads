b8fe4353955db689a5f4d18ea0b062d1cfb4d0aa
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-11-05 10:13:50 +0530

    Add files via upload

---
+++ LambdaQ4.java
@@ -0,0 +1,19 @@
+package p7;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LambdaQ4 {
+  public static void main(String[] args) {
+
+    ArrayList<String> list1 = new ArrayList<String>(Arrays.asList("hello", "world", "good", "morning", "zoom"));
+    System.out.println(list1);
+    for (String word : list1)
+      System.out.println(word);
+    System.out.println("the words with the even number length:");
+    list1.removeIf(word -> ((word.length() % 2) != 0));
+    for (String word : list1) {
+      System.out.println(word);
+    }
+  }
+}
