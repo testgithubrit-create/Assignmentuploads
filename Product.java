a86e6f9829d6035f8713f0d583127aa0dc993735
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-11-05 10:04:47 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,21 @@
+package p5;
+
+import java.util.HashSet;
+
+public class Product {
+  public static void main(String[] args) {
+
+    HashSet<String> obj = new HashSet<>();
+    obj.add("Cups");
+    obj.add("chairs");
+    obj.add("pens");
+    obj.add("table");
+    obj.add("Books");
+    obj.add("papers");
+    obj.add("flank");
+    obj.add("breshes");
+    obj.add("colors");
+    obj.add("mobiles");
+    System.out.println(obj);
+  }
+}
