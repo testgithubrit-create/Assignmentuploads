c14857655cbf2557e124b32dcb68558cea2f2b22
testgithubrit-create <115694019+testgithubrit-create@users.noreply.github.com>
2022-11-05 09:37:33 +0530

    Add files via upload

---
+++ HandlingException.java
@@ -0,0 +1,19 @@
+package p3;
+
+public class HandlingException {
+  public static void main(String args[]) {
+
+    int a, b, res;
+
+    a = 10;
+    b = 0;
+    try {
+      res = a / b;
+      System.out.println("Result: " + res);
+
+    } catch (ArithmeticException e) {
+
+      throw new ArithmeticException("UnsupportedOperationException");
+    }
+  }
+}
